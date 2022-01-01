b0fc8d826d5538a53a514303586eaa1a611ce34f
clovett <lovettchris@hotmail.com>
2021-12-31 23:09:22 -0800

    Publish version 1.1.0.81.

--- Source/WPF/Version/Version.cs
+++ Source/WPF/Version/Version.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 // And also sync this with the MSIX package manifest in
 // ~\MyMoney.Net\Source\WPF\MoneyPackage\Package.appxmanifest
 
-[assembly: AssemblyVersion("1.1.0.80")]
-[assembly: AssemblyFileVersion("1.1.0.80")]
+[assembly: AssemblyVersion("1.1.0.81")]
+[assembly: AssemblyFileVersion("1.1.0.81")]
