0644eb94e4fc809e67b7501a58b62d40125e14b3
clovett <lovettchris@hotmail.com>
2022-09-29 22:14:42 -0700

    publish 2.0.1.0

--- Source/WPF/Version/Version.cs
+++ Source/WPF/Version/Version.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 // And also sync this with the MSIX package manifest in
 // ~\MyMoney.Net\Source\WPF\MoneyPackage\Package.appxmanifest
 
-[assembly: AssemblyVersion("2.0.0.20")]
-[assembly: AssemblyFileVersion("2.0.0.20")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
