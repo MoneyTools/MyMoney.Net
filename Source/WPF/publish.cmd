0a22a6918550c27d03b7be32fdd665e63c1ed4b7
clovett <lovettchris@hotmail.com>
2022-11-13 14:11:25 -0800

    automate the publish build steps

--- Source/WPF/publish.cmd
+++ Source/WPF/publish.cmd
@@ -11,6 +11,7 @@ echo ### Publishing version %VERSION%...
 set WINGET=1
 set GITRELEASE=1
 set UPLOAD=1
+set ClickOnceBits=MyMoney\publish
 
 :parse
 if "%1"=="/nowinget" set WINGET=0
@@ -23,8 +24,13 @@ goto :parse
 :done
 
 if "%LOVETTSOFTWARE_STORAGE_CONNECTION_STRING%" == "" goto :nokey
-if not EXIST publish goto :nobits
 
+if EXIST %ClickOnceBits% rd /s /q %ClickOnceBits%
+msbuild /target:publish MyMoney.sln /p:Configuration=Release "/p:Platform=Any CPU" /p:PublishDir=publish
+if not EXIST %ClickOnceBits%\MyMoney.application goto :nopub
+
+if EXIST MoneyPackage\AppPackages rd /s /q MoneyPackage\AppPackages
+msbuild /target:publish MyMoneyPackage.sln /p:Configuration=Release "/p:Platform=Any CPU"
 if not EXIST MoneyPackage\AppPackages\MoneyPackage_%VERSION%_Test\MoneyPackage_%VERSION%_AnyCPU.msixbundle goto :noappx
 
 if "%GITRELEASE%" == "0" goto :upload
@@ -40,8 +46,8 @@ del notes.txt
 :upload
 if "%UPLOAD%" == "0" goto :dowinget
 echo Uploading ClickOnce installer
-copy /y MyMoney\Setup\changes.xml publish
-call AzurePublishClickOnce %ROOT%publish downloads/MyMoney "%LOVETTSOFTWARE_STORAGE_CONNECTION_STRING%"
+copy /y MyMoney\Setup\changes.xml %ClickOnceBits%
+call AzurePublishClickOnce %ROOT%%ClickOnceBits% downloads/MyMoney "%LOVETTSOFTWARE_STORAGE_CONNECTION_STRING%"
 call AzurePublishClickOnce %ROOT%MoneyPackage\AppPackages downloads/MyMoney.Net "%LOVETTSOFTWARE_STORAGE_CONNECTION_STRING%"
 
 echo ============ Done publishing ClickOnce installer ==============
@@ -103,14 +109,14 @@ goto :eof
 echo Please set your LOVETTSOFTWARE_STORAGE_CONNECTION_STRING
 exit /b 1
 
-:nobits
-echo 'publish' folder not found, please run Solution/Publish first.
-exit /b 1
-
-:noappx
-echo Please build the .msixbundle using MoneyPackage.wapproj project, publish/create appx packages.
-exit /b 1
-
 :nowinget
 echo Please clone git@github.com:lovettchris/winget-pkgs.git into %WINGET_SRC%
 exit /b 1
+
+:noappx
+echo Could not find MoneyPackage\AppPackages\MoneyPackage_%VERSION%_Test\MoneyPackage_%VERSION%_AnyCPU.msixbundle
+exit /b /1
+
+:nopub
+echo Could not find %ClickOnceBits%\MyMoney.application
+exit /b /1
