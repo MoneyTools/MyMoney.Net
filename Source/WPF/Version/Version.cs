27d59143c47e5fc48b95f3089413070d4197ea13
clovett <lovettchris@hotmail.com>
2024-07-07 00:36:02 -0700

    Fix missing resource in light theme.

--- Source/WPF/Version/Version.cs
+++ Source/WPF/Version/Version.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 // And also sync this with the MSIX package manifest in
 // ~\MyMoney.Net\Source\WPF\MoneyPackage\Package.appxmanifest
 
-[assembly: AssemblyVersion("2.1.0.23")]
-[assembly: AssemblyFileVersion("2.1.0.23")]
+[assembly: AssemblyVersion("2.1.0.24")]
+[assembly: AssemblyFileVersion("2.1.0.24")]
